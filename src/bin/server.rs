9ecc66ae81f1800ec25dead8ac968a0ae20e9a54
Justin Maillet <justy777@gmail.com>
2022-05-06 20:06:17 -0300

    Add thread pool to library

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,9 @@
 use std::io;
 use std::net::{TcpListener, TcpStream, ToSocketAddrs, UdpSocket};
-use std::thread;
 
 use clap::Parser;
 use echo_protocol::io::{BufTcpStream, MAX_DATAGRAM_SIZE};
+use echo_protocol::thread_pool::ThreadPool;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -33,12 +33,15 @@ fn main() -> io::Result<()> {
 
 fn handle_tcp<A: ToSocketAddrs>(address: A) -> io::Result<()> {
     let listener = TcpListener::bind(&address)?;
+    let pool = ThreadPool::new(num_cpus::get());
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(move || {
-                    handle_connection(stream).map_err(|e| eprintln!("Error: {}", e))
+                pool.execute(move || {
+                    if let Err(e) = handle_connection(stream) {
+                        eprintln!("Error: {}", e);
+                    }
                 });
             }
             Err(e) => eprintln!("Failed to accept connection: {}", e),
